cebda516beb0f34327939779a40e3e05e954240b
akash1309 <akash.goyal1309@gmail.com>
2018-05-27 20:14:51 +0530

    schema vendor fields standard and vendor addition

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,20 @@
 //controllers
 const ceeSignup = require('./../controllers/ceeSignup');
 const dyceeSignup = require('./../controllers/dyceeSignup');
+<<<<<<< Updated upstream
 const inspectorAdd = require('./../controllers/inspectorAdd');
+=======
+const vendorSignup = require('./../controllers/vendorSignup');
+
+>>>>>>> Stashed changes
 
 module.exports = function(app, db) {
   app.post('/cee/signup', ceeSignup);
   app.post('/dycee/signup',dyceeSignup);
+<<<<<<< Updated upstream
   app.post('/inspector/add', inspectorAdd);
+=======
+  app.post('/vendor/signup',vendorSignup);
+
+>>>>>>> Stashed changes
 }
