998f94edbbffccf849d6853211733b7fed23b956
Shobha Goyal <shobha4goyal@gmail.com>
2018-06-02 17:13:10 +0530

    getRoutes added

---
+++ app/controllers/getPurchaseOrder.js
@@ -0,0 +1,26 @@
+const { PurchaseOrder } = require('./../models/Schema.js');
+
+exports.findOne = (req,res) => {
+	
+	PurchaseOrder.find({order_number : req.params.order_number})
+	.then( purchaseOrderInfo => {
+			
+		if(!purchaseOrderInfo) {
+			return res.status(400).send({
+				message : "No purchase order found with order number "+req.params.order_number
+			});
+		}		
+		res.send(purchaseOrderInfo);
+	})
+	.catch(err => {
+		
+		if(err.kind == 'ObjectId'){
+			return res.status(400).send({
+				message : "No purchase order found with order number "+req.params.order_number
+			});
+		}
+		return res.status(500).send({
+			message : "Some error occured while extracting purchase order with order number "+req.params.order_number
+		});
+	});
+}
--- app/controllers/inspectorAdd.js
+++ app/controllers/inspectorAdd.js
@@ -6,8 +6,9 @@ module.exports = function(req, res){
   var newInspector = Inspector({
     email:    req.body.email,
     name:     req.body.name,
-    mobile: req.body.mobile,
-    dycee_id:   req.body.dycee_id
+    mobile:   req.body.mobile,
+    dycee_id: req.body.dycee_id,
+    cee_id:   req.body.cee_id
   });
 
   Inspector.findOne({
