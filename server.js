0650429a2cab2a4da014f96daa1e67d5318dcf3b
AnshuGoyal <anshu.goyal9194@gmail.com>
2018-06-02 19:27:54 +0530

    Port Change

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // MongoDB Connection using mongoose
 mongoose.connect(db.url, (err, database) => {
   if (err) return console.log(err)
-  
+
   require('./app/routes')(app, database);
 
-  server.listen(80, (err) => {
+  server.listen(8080, (err) => {
   	if ( ! err) {
   		console.log(`server is listening on 80`)
   	}
