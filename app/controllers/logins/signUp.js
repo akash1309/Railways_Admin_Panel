b8f0dfa88785ee10ea228e96e7c192717b37b093
AnshuGoyal <anshu.goyal9194@gmail.com>
2018-06-05 23:05:32 +0530

    Password Correction

--- app/controllers/logins/signUp.js
+++ app/controllers/logins/signUp.js
@@ -4,36 +4,29 @@ exports.findOne = (req,res) => {
 
 	Logins.findOne({mobile : req.params.mobile})
 	.then(LoginsInfo => {
-
-		if(LoginsInfo.length == 0) {
-            return res.status(404).send({
-                "message": "No one is registered with this mobile number."
-        });
-    }
-    if(LoginsInfo.password.length == 0) {
+    if(LoginsInfo.password == undefined) {
       return res.status(200).send({
-            "message": "Password field empty. Requires SignUp",
-            "role": LoginsInfo.role,
-            "flag": "0"
+        "message": "Password field empty. Requires SignUp",
+        "role": LoginsInfo.role,
+        "flag": "0"
       });
     }
     return res.status(200).send({
-          "message": "Entry present.",
-          "role": LoginsInfo.role,
-          "flag": "1"
-	});
-})
+      "message": "Entry present.",
+      "role": LoginsInfo.role,
+      "flag": "1"
+		});
+	})
 	.catch(err => {
 		if(err.kind == 'ObjectId') {
-            return res.status(404).send({
-                "message": "No one is registered with this mobile number."
-            });
-        }
-
+      return res.status(404).send({
+        "message": "No one is registered with this mobile number."
+      });
+    }
 		return res.status(500).send({
 			"message": "Some error occurred while getting login information.",
       "error": err
 		});
-
 	});
+	
 }
